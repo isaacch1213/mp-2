ccc89802125798ee1f1c25a05c4dfd9b131a8c72
Isaac Chan <isaacch@bu.edu>
2025-09-30 18:20:56 -0400

    updated fetch to be conventional standard

--- src/App.tsx
+++ src/App.tsx
@@ -18,50 +18,35 @@ export default function App() {
 
   useEffect(() => {
     async function fetchData(): Promise<void> {
-      try {
-        const res = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=151");
-        if (!res.ok) {
-          throw new Error(`Response status: ${res.status}`);
-        }
-        const data = await res.json();
-
-        const pokemonDetails: Pokemon[] = await Promise.all(
-          data.results.map(async (pokemon: PokemonNameUrl) => {
-            try {
-              const res = await fetch(pokemon.url);
-              if (!res.ok) {
-                throw new Error(`Response status: ${res.status}`);
-              }
-              const detail = await res.json();
-              return {
-                id: detail.id,
-                name: pokemon.name,
-                front_default: detail.sprites.front_default,
-                types: detail.types.map((t: PokemonTypeInfo) => t.type.name),
-                weight: detail.weight,
-              };
-            } catch (error) {
-              if (error instanceof Error) {
-                console.error("Message:", error.message);
-              } else {
-                console.log(
-                  "Something unexpected occured in the error:",
-                  error,
-                );
-              }
-            }
-          }),
-        );
-        setData(pokemonDetails);
-      } catch (error) {
-        if (error instanceof Error) {
-          console.error("Message:", error.message);
-        } else {
-          console.log("Something unexpected occured in the error:", error);
-        }
+      const res = await fetch("https://pokeapi.co/api/v2/pokemon/?limit=151");
+      if (!res.ok) {
+        throw new Error(`Response status: ${res.status}`);
       }
+      const data = await res.json();
+
+      const pokemonDetails: Pokemon[] = await Promise.all(
+        data.results.map(async (pokemon: PokemonNameUrl) => {
+          const res = await fetch(pokemon.url);
+          if (!res.ok) {
+            throw new Error(`Response status: ${res.status}`);
+          }
+          const detail = await res.json();
+          return {
+            id: detail.id,
+            name: pokemon.name,
+            front_default: detail.sprites.front_default,
+            types: detail.types.map((t: PokemonTypeInfo) => t.type.name),
+            weight: detail.weight,
+          };
+        }),
+      );
+
+      setData(pokemonDetails);
     }
-    fetchData();
+
+    fetchData()
+      .then(() => console.log("Data fetched sucessfully"))
+      .catch((e: Error) => console.log("There was an error:", e.message));
   }, []);
 
   return (
